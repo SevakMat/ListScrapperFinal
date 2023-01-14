273f6d31b18147be66ef1b93d2e99e8f9b3da860
SevakMat <sevakmatevosyan98@gmail.com>
2023-01-14 19:00:03 +0400

    added function for save info in db

--- front/src/services/scrapping.service.ts
+++ front/src/services/scrapping.service.ts
@@ -2,5 +2,5 @@ import axios from "../config/axios"
 import { ISearchForm } from "../components/dashboard/Dashboard"
 
 export const SendCarData = async (searchForm: ISearchForm, userId: string): Promise<any> => {
-  return axios.post("api/v1/scrapping/add-car", { ...searchForm, userId })
+  return axios.post("api/v1/scrapping/add-car", { "filterInfo":searchForm, "userId":userId })
 }
